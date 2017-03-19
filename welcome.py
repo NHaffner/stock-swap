8fa41a421253c5a2763be6c235788abe64c8bd67
NHaffner <nataliehaffner@hotmail.com>
2017-03-19 13:39:03 +0200

    first heroku

--- welcome.py
+++ welcome.py
@@ -51,3 +51,48 @@ def Welcome():
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(port))
+    # app.run(host='127.0.0.1', port=int(port))
+
+
+
+
+
+# import json
+# import urllib.request, urllib.error, urllib.parse
+#
+# APPLICATION_ID = 'ede14ddd'
+# APPLICATION_KEY = 'e9ae2130310ebe48fa5df1e6ae322bfa'
+#
+# def call_api(endpoint, parameters):
+#   url = 'https://api.aylien.com/api/v1/' + endpoint
+#   headers = {
+#       "Accept":                             "application/json",
+#       "Content-type":                       "application/x-www-form-urlencoded",
+#       "X-AYLIEN-TextAPI-Application-ID":    "ede14ddd",
+#       "X-AYLIEN-TextAPI-Application-Key":   "e9ae2130310ebe48fa5df1e6ae322bfa",
+#   }
+#   opener = urllib.request.build_opener()
+#   request = urllib.request.Request(url,
+#     urllib.parse.urlencode(parameters).encode('utf-8'), headers)
+#   response = opener.open(request);
+#   return json.loads(response.read().decode())
+#
+# parameters = {"text": "What language is this sentence written in?"}
+# language = call_api("language", parameters)
+#
+# parameters = {"text": "John is a very good football player!"}
+# sentiment = call_api("sentiment", parameters)
+#
+# parameters = {"url": "http://www.bbc.com/news/health-29912877"}
+# hashtags = call_api("hashtags", parameters)
+#
+# print("n")
+# print("Text: %s " % (language["text"]))
+# print("Language: %s (%F)" % (language["lang"], language["confidence"]))
+# print("n")
+# print("Text: %s " % (sentiment["text"]))
+# print("Sentiment: %s (%F)" % (sentiment["polarity"], sentiment["polarity_confidence"]))
+# print("n")
+# print("Hashtags: %s " % (hashtags["hashtags"]))
+# print("n")
+# print("Text: %s " % (hashtags["text"]))
