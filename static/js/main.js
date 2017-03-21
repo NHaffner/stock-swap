30fe6c9fec210bb1dee69f7a93d49de1e5e7afa9
rebeccaemmerich <rebeccaemmerich@gmail.com>
2017-03-21 19:53:25 +0200

    merged

---
+++ static/js/main.js
@@ -0,0 +1,138 @@
+$(document).ready(function() {
+
+var search = function(){
+    $("#spot").show();
+    var input = $("#searchfield").val()
+    $.ajax({
+             type:'POST',
+             url:'/stock',
+             data:{"input":input},
+             success: function(response) {
+                console.log(response)
+
+             $("#searched").html(response)
+             $("#name").text(response.result.input1)
+             $("#result").text(response.result.sentiment.polarity)
+             $("#summary").text(response.result.summarize.sentences)
+             // $("#polarity").text(result.sentiment.polarity);
+              },
+            error:function(response){
+                console.log("error"+response)
+            }
+
+});
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "main") {
+        x.className += " responsive";
+    } else {
+        x.className = "main";
+    }
+}
+
+//
+$("#image1").hover(function(){
+    $('.content1').show();
+},function(){
+    $('.content1').hide();
+});
+
+$("#image2").hover(function(){
+    $('.content2').show();
+},function(){
+    $('.content2').hide();
+});
+
+$("#image3").hover(function(){
+    $('.content3').show();
+},function(){
+    $('.content3').hide();
+});
+
+$("#image4").hover(function(){
+    $('.content4').show();
+},function(){
+    $('.content4').hide();
+});
+
+$("#image5").hover(function(){
+    $('.content5').show();
+},function(){
+    $('.content5').hide();
+});
+
+$("#image6").hover(function(){
+    $('.content6').show();
+},function(){
+    $('.content6').hide();
+});
+
+$("#image7").hover(function(){
+    $('.content7').show();
+},function(){
+    $('.content7').hide();
+});
+
+$("#image8").hover(function(){
+    $('.content8').show();
+},function(){
+    $('.content8').hide();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
+function formatMonthlyData(data) {
+  var monthlyData = [];
+
+  for (var i = 0; i < data.length; i++) {
+    monthlyData.push({x: data[i].Month, y: +data[i].Data});
+  }
+
+  return monthlyData;
+}
+
+
+$(".search").keypress(function(e) {
+if(e.which == 13) {
+
+    search();
+
+    var searchstring = $('#searchfield');
+    searchstring.focus();
+    var xyz = searchstring.val();
+    console.log(xyz);
+    var url = "https://twitter.com/" + xyz;
+    $(document).ready(function() {
+        
+        $("#ourdiv").html("<a class=\"twitter-timeline\" data-width=\"500\" data-height=\"300\" id=\"twittersearcher\" href=\"" + url + "\" >Tweets by TwitterDev</a> <script async src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>")
+        var n = $(document).height();
+        $('html, body').animate({scrollTop: n}, 500);
+        })
+
+    }
+})
+
+
+
+});
+
+
--- welcome.py
+++ welcome.py
@@ -1,70 +1,24 @@
-from aylienapiclient import textapi
+#Copyright 2015 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
-from flask import Flask
+from flask import Flask, jsonify,request
 import json
 import webbrowser
 import json
 import cgi
 import urllib.error, urllib.parse
-import blockspring
-
-client = textapi.Client("ede14ddd", "e9ae2130310ebe48fa5df1e6ae322bfa")
-
-sentimentRaw = client.Sentiment({'text': 'John is a very good football player!'})
-
-# print sentimentRaw
-
-print "\n******************************** Sentiment Analysis \n"
-
-url = 'http://edition.cnn.com/2017/03/14/europe/netherlands-refugees-wilders-we-are-here/index.html'
-sentimentURL = client.Sentiment({'url': url})
-for key, value in sentimentURL.iteritems():
-    if not key == 'text':
-         print key, " : ", value
-
-
-print "\n******************************** Summarize \n"
-
-summary = client.Summarize({'url': url, 'sentences_number': 3})
-for sentence in summary['sentences']:
-  print sentence
-
-print "\n******************************** Entities \n"
-
-entities = client.Entities({"url": url})
-for type, values in entities['entities'].iteritems():
-  print type,', '.join(values)
-
-print "\n******************************** Hashtags\n"
-
-hashtags = client.Hashtags({"url": url})
-print ', '.join(hashtags['hashtags'])
-
-print "\n******************************** Classifying by Taxonomy\n"
-
-classifications = client.ClassifyByTaxonomy({"url": url, "taxonomy": "iab-qag"})
-for category in classifications['categories']:
-  print category
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def Welcome():
-    return app.send_static_file('index.html')
-
-@app.route('/search')
-def Search():
-    return app.send_static_file('search.html')
-
-port = os.getenv('PORT', '5000')
-if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=int(port))
-    app.run(host='127.0.0.1', port=int(port))
-
-
-
-
 
 
 APPLICATION_ID = "67c4cb60"
@@ -95,7 +49,6 @@ def Welcome():
 def Stock():
   input1=str(request.form['input'])
 
-  link = "http://money.cnn.com/quote/quote.html?symb=FIT";
   print("user input",input1)
 
   parameters = {"url": input1}
@@ -108,48 +61,14 @@ def Stock():
   hashtags = call_api("hashtags", parameters)
 
   parameters = {"url": input1}
-  summarize = call_api("summarize", parameters)
+  summarize = call_api("summarize", parameters) 
 
 
-  info = {'language':language,'sentiment':sentiment,'hashtags':hashtags, 'input1':input1}
+  info = {'language':language,'sentiment':sentiment,'hashtags':hashtags, 'summarize': summarize, 'input1':input1}
 
-  #return app.send_static_file('stock.html')
-  #return jsonify(result=json.dumps(info))
   return jsonify(result=info)
 
-# get link variable from js
-# @app.route('/postmethod', methods = ['POST'])
-# def getData():
-#     jsdata = request.form['javascript_data']
-#     return jsdata
 
-
-# @app.route('/search', methods = ['GET', 'POST'])
-# def Search():
-# #	return app.send_static_file('stock.html')
-#   # link="jgjgjg"
-#  # link = request.POST.get('javascript_data')
-#  # print (link)
-#   link = "http://money.cnn.com/quote/quote.html?symb=FIT";
-
-#   parameters = {"url": link}
-#   language = call_api("language", parameters)
-
-#   parameters = {"url": link}
-#   sentiment = call_api("sentiment", parameters)
-
-#   parameters = {"url": link}
-#   hashtags = call_api("hashtags", parameters)
-
-
-
-#   info = {'language':language,'sentiment':sentiment,'hashtags':hashtags}
-
-#   return json.dumps(info)
-
-def getUserInput():
-  form = web.input(name="Search")
-  greeting = "%s" % (form.name)
- # return render.index(name = name)
-  return app.send_static_file('stock.html')
-
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=int(port))
